52958a03c7ce6a0e545d18606d4f51c1134728fa
looph0le <mitanshpanchal@gmail.com>
2023-01-31 11:26:41 +0530

    added HandTracking and FaceMesh Modules

---
+++ ISCV/FaceMash.py
@@ -0,0 +1,71 @@
+import cv2
+import mediapipe as mp
+import math
+
+
+class FaceMeshDetector:
+
+    def __init__(self, staticMode=False, maxFaces=2, minDetectionCon=0.5, minTrackCon=0.5):
+
+        self.staticMode = staticMode
+        self.maxFaces = maxFaces
+        self.minDetectionCon = minDetectionCon
+        self.minTrackCon = minTrackCon
+
+        self.mpDraw = mp.solutions.drawing_utils
+        self.mpFaceMesh = mp.solutions.face_mesh
+        self.faceMesh = self.mpFaceMesh.FaceMesh(static_image_mode=self.staticMode,
+                                                 max_num_faces=self.maxFaces,
+                                                 min_detection_confidence=self.minDetectionCon,
+                                                 min_tracking_confidence=self.minTrackCon)
+        self.drawSpec = self.mpDraw.DrawingSpec(thickness=1, circle_radius=2)
+
+    def findFaceMesh(self, img, draw=True):
+
+        self.imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.faceMesh.process(self.imgRGB)
+        faces = []
+        if self.results.multi_face_landmarks:
+            for faceLms in self.results.multi_face_landmarks:
+                if draw:
+                    self.mpDraw.draw_landmarks(img, faceLms, self.mpFaceMesh.FACEMESH_CONTOURS,
+                                               self.drawSpec, self.drawSpec)
+                face = []
+                for id, lm in enumerate(faceLms.landmark):
+                    ih, iw, ic = img.shape
+                    x, y = int(lm.x * iw), int(lm.y * ih)
+                    face.append([x, y])
+                faces.append(face)
+        return img, faces
+
+    def findDistance(self,p1, p2, img=None):
+
+        x1, y1 = p1
+        x2, y2 = p2
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+        length = math.hypot(x2 - x1, y2 - y1)
+        info = (x1, y1, x2, y2, cx, cy)
+        if img is not None:
+            cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x2, y2), 15, (255, 0, 255), cv2.FILLED)
+            cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+            cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+            return length,info, img
+        else:
+            return length, info
+
+
+def main():
+    cap = cv2.VideoCapture(0)
+    detector = FaceMeshDetector(maxFaces=2)
+    while True:
+        success, img = cap.read()
+        img, faces = detector.findFaceMesh(img)
+        #if faces:
+            #print(faces[0])
+        cv2.imshow("Image", img)
+        cv2.waitKey(1)
+
+
+if __name__ == "__main__":
+    main()
